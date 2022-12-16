0ada9ccb6185dc5195dca0fb4580d3e705b7ce36
Adam Gartee <agartee@gmail.com>
2022-12-16 05:45:52 -0500

    added additional cli command to demonstrate noun-verb config for commandlineparser library integration.

---
+++ src/Requests/CreateUser.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using MediatR;
+
+namespace TestIdentityServer.Requests
+{
+    [Verb("create-user", HelpText = "create user: Create a new user.")]
+    public class CreatePerson : IRequest<string>
+    {
+        [Option(Required = true, HelpText = "name")]
+        public string? Name { get; set; }
+    }
+
+    public class CreatePersonHandler : IRequestHandler<CreatePerson, string>
+    {
+        public Task<string> Handle(CreatePerson request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult("User created!");
+        }
+    }
+}
